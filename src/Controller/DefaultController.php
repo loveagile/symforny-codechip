c62ab2d878dfd663129c46e90e03952c958016a4
loveagile <happyplandry@gmail.com>
2020-11-23 12:46:28 -0300

    Insert in database for test.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
 
 class DefaultController extends AbstractController
 {
@@ -14,6 +15,21 @@ class DefaultController extends AbstractController
     public function index(): Response
     {
         $name = 'Lwcyano Will';
+
+        //Criando dados com Doctrine
+        $product = new Product();
+        $product->setName('Produto Teste');
+        $product->setDescription('Descrição');
+        $product->setBody('Info produto');
+        $product->setSlug('produto-test');
+        $product->setPrice(1990);
+        $product->setCreatedAt(new \DateTime('now', new \DateTimeZone('America/Sao_Paulo')));
+        $product->setUpdateAt(new \DateTime('now', new \DateTimeZone('America/Sao_Paulo')));
+
+        $manager = $this->getDoctrine()->getManager();
+        $manager->persist($product);
+        $manager->flush();
+
         return $this->render('index.html.twig', compact('name'));
     }
 
