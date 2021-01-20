b7d2462f43dea49912afaa03749b0e60f89dd32c
loveagile <happyplandry@gmail.com>
2021-01-20 00:05:28 -0300

    99 - Removing images.

---
+++ src/Controller/ProductPhotoController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\ProductPhoto;
+use Doctrine\ORM\EntityManagerI;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductPhotoController extends AbstractController
+{
+    /**
+     * @Route("/product/photo/{productPhoto}", name="admin_product_photo_remove")
+     */
+    public function remove(ProductPhoto $productPhoto, EntityManagerInterface $em)
+    {
+        $product = $productPhoto->getProduct()->getId();
+        $realImage = $this->getParameter('upload_dir') . '/products/' . $productPhoto->getPhoto();
+
+        $em->remove($productPhoto);
+        $em->flush();
+
+        if(file_exists($realImage))
+            unlink($realImage);
+
+        return $this->redirectToRoute('admin_edit_products', ['product' => $product]);
+
+    }
+}
