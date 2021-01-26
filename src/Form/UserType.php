89a6a74fe2be28d2653cb143a43aa64bb9b10180
loveagile <happyplandry@gmail.com>
2021-01-26 18:54:30 -0300

    Create form to entity user.

---
+++ src/Form/UserType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('email')
+            ->add('password')
+            ->add('roles')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('address')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
