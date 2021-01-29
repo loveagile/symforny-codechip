7e61512554a78a6571a515d698cf3a121fbcd06d
loveagile <happyplandry@gmail.com>
2021-01-29 11:13:25 -0300

    122 - Conclusions.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -28,7 +28,7 @@ class SecurityController extends AbstractController
     /**
      * @Route("/logout", name="app_logout")
      */
-    public function logout(): Response
+    public function logout()
     {
     }
 }
