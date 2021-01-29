1bd0b039b92ad4b7cbac42a3d25d01c3e7d4999e
loveagile <happyplandry@gmail.com>
2021-01-29 11:56:56 -0300

    124 - Working Controller Home and Single.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,78 +15,18 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index( ProductRepository $productRepository)
     {
-        $name = 'Lwcyano Will';
-        $user = $this->getDoctrine()->getRepository(User::class)->find(1);
-        //$order = $this->getDoctrine()->getRepository(Order::class)->find(1);
-        //dump($user->getOrder()->toArray());
-        //$user->removeOrder($order);
-
-        $this->getDoctrine()->getManager()->flush();
-
-        //Produto e Categoria
-        //$category = $this->getDoctrine()->getRepository(Category::class)->find(1);
-        //dump($category->getProducts()->toArray());
-
-//        $category = new Category();
-//        $category->setName('Notebooks');
-//        //$category->setDescription('Notebooks e Netbooks');
-//        $category->setSlug('notebook');
-//        $category->setCreatedAt(new \DateTime('now', new \DateTimeZone('America/Sao_Paulo')));
-//        $category->setUpdatedAt(new \Datetime('now', new \DateTimeZone('America/Sao_Paulo')));
-//
-//        $product->setCategory($category);
-//        $this->getDoctrine()->getManager()->flush();
-
-        //$order = $this->getDoctrine()->getRepository(Order::class)->find(1);
-        //dump($order->getUser()->getFirstName());
-
-//        $order = new Order();
-//        $order->setReference('CODIDO COMPRA TRÊS');
-//        $order->setItems('ITEMS');
-//        $order->setUser($user);
-//
-//        $order->setCreatedAt(new \DateTime('now', new \DateTimeZone('America/Sao_Paulo')));
-//        $order->setUpdatedAt(new \DateTime('now', new \DateTimeZone('America/Sao_Paulo')));
-//        $order->setUser($user);
-//        //dump($order);
-//
-//        $this->getDoctrine()->getManager()->persist($order);
-//        $this->getDoctrine()->getManager()->flush($order);
-        //dump($address->getUser()->getLastName());
-
-        /*$user = new User();
-        $user->setFirstName('Harry');
-        $user->setLastName('Will');
-        $user->setEmail('willvix@outlook.com');
-        $user->setPassword('10');
-        $user->setCreatedAt(new \DateTime('now', new \DateTimeZone('America/Sao_Paulo')));
-        $user->setUpdateAt(new \DateTime('now', new \DateTimeZone('America/Sao_Paulo')));
-
-        $manager->persist($user);
-        $manager->flush();
-
-        $address = new Address();
-        $address->setAddress('Rua Holdecim');
-        $address->setNumber(840);
-        $address->setNeighborhood('Civit 2');
-        $address->setCity('Serra');
-        $address->setState('ES');
-        $address->setZipcode('29168-066');
-        $address->setUser($user);
-
-        $manager->persist($address);
-        $manager->flush();*/
-
-        return $this->render('index.html.twig', compact('name','user'));
+        $products = $productRepository->findAll();
+        dump($products);
+        return $this->render('index.html.twig', compact('products'));
     }
 
     /**
-     * @Route("/product/{slug}", name="product_single")
+     * @Route("/{slug}", name="product_single")
      */
-    public function product($slug): Response
+    public function product(Product $product)
     {
-        return $this->render('single.html.twig', compact('slug'));
+        return $this->render('single.html.twig', compact('product'));
     }
 }
