b7bc313d48170b3327afd45d854caf15243bfa9f
loveagile <happyplandry@gmail.com>
2021-02-01 17:57:13 -0300

    140 - Starting the Checkout Process.

---
+++ src/Controller/CheckoutController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CheckoutController extends AbstractController
+{
+    /**
+     * @Route("/checkout", name="home_checkout", priority="10")
+     * @IsGranted("ROLE_USER")
+     */
+    public function index(Request $request)
+    {
+        $session = $request->getSession();
+        if(!$session->has('cart')) {
+            return $this->redirectToRoute('home');
+        }
+
+        $cart = $session->get('cart');
+
+        return $this->render('checkout.html.twig', compact('cart'));
+    }
+}
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/login", name="app_login", priority="10")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -26,7 +26,7 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("/logout", name="app_logout")
+     * @Route("/logout", name="app_logout", priority="10")
      */
     public function logout()
     {
