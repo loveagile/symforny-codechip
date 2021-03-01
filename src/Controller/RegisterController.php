59c92db40bf5c7fd580a76edc3fd70c690799fb7
loveagile <happyplandry@gmail.com>
2021-03-01 17:12:03 -0300

    144 - Authenticating Post-registration User.

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Form\UserRegisterFormType;
+use App\Security\LoginFormAuthenticator;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,13 +11,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 
 class RegisterController extends AbstractController
 {
     /**
      * @Route("/register", name="home_register", priority="10")
      */
-    public function index(Request $request, UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $em)
+    public function index(Request $request, UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $em, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $loginForm)
     {
         $form = $this->createForm(UserRegisterFormType::class);
         $form->handleRequest($request);
@@ -29,6 +31,7 @@ class RegisterController extends AbstractController
             $em->persist($user);
             $em->flush();
 
+            return $guardHandler->authenticateUserAndHandleSuccess($user, $request, $loginForm, 'main');
             dd($user);
         }
 
