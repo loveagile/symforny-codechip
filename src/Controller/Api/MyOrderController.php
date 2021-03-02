50be5979ad889440c6299bdccfcca35301a9e245
loveagile <happyplandry@gmail.com>
2021-03-02 10:52:03 -0300

    150 - Introduction.

---
+++ src/Controller/Api/MyOrderController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MyOrderController extends AbstractController
+{
+    /**
+     * @Route("/api/my/order", name="api_my_order")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/Api/MyOrderController.php',
+        ]);
+    }
+}
